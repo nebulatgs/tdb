f941f5565e08427fee24f00cca230b2ec00972b9
nebulatgs <nebula@kora.gg>
2024-07-14 20:31:25 -0400

    add postinstall

--- postinstall.js
+++ postinstall.js
@@ -1,107 +1,54 @@
 // @ts-check
 
-import * as fs from "fs";
-import * as https from "https";
-import * as lzmajs from "lzma-purejs";
+import { existsSync } from "fs";
+import path from "path";
+import { extract } from "tar";
+import XzDecompress from "xz-decompress";
+const { XzReadableStream } = XzDecompress;
 
-// Determine package name for this platform
-const binaryPackageName = "@nebulatgs/tdb-filesystem";
-const BINARY_DISTRIBUTION_VERSION = "1.0.0";
-function makeRequest(url) {
-	return new Promise((resolve, reject) => {
-		https
-			.get(url, (response) => {
-				if (response.statusCode >= 200 && response.statusCode < 300) {
-					const chunks = [];
-					response.on("data", (chunk) => chunks.push(chunk));
-					response.on("end", () => {
-						resolve(Buffer.concat(chunks));
-					});
-				} else if (
-					response.statusCode >= 300 &&
-					response.statusCode < 400 &&
-					response.headers.location
-				) {
-					// Follow redirects
-					makeRequest(response.headers.location).then(resolve, reject);
-				} else {
-					reject(
-						new Error(
-							`npm responded with status code ${response.statusCode} when downloading the package!`
-						)
-					);
-				}
-			})
-			.on("error", (error) => {
-				reject(error);
+const DATA_DIR_VERSION = "v0.0.1-alpha";
+const unpackPath = path.join(path.dirname(import.meta.path), "src");
+
+async function downloadBinaryFromGitHub() {
+	try {
+		const tarballDownloadBuffer = await fetch(
+			`https://github.com/nebulatgs/tdb/releases/download/${DATA_DIR_VERSION}/data.txz`
+		)
+			.then((response) => new Response(new XzReadableStream(response.body)))
+			.then((response) => response.arrayBuffer());
+		const tarballBuffer = Buffer.from(tarballDownloadBuffer);
+		const unpack = extract({
+			cwd: unpackPath,
+			sync: false,
+		});
+
+		/** @type {Promise<void>} */
+		const unpackPromise = new Promise((resolve) => {
+			unpack.write(tarballBuffer);
+			unpack.end();
+			unpack.once("end", () => {
+				console.log("Binary downloaded and unpacked successfully.");
+				resolve();
 			});
-	});
-}
-
-function extractFileFromTarball(tarballBuffer, filepath) {
-	// Tar archives are organized in 512 byte blocks.
-	// Blocks can either be header blocks or data blocks.
-	// Header blocks contain file names of the archive in the first 100 bytes, terminated by a null byte.
-	// The size of a file is contained in bytes 124-135 of a header block and in octal format.
-	// The following blocks will be data blocks containing the file.
-	let offset = 0;
-	while (offset < tarballBuffer.length) {
-		const header = tarballBuffer.subarray(offset, offset + 512);
-		offset += 512;
-
-		const fileName = header.toString("utf-8", 0, 100).replace(/\0.*/g, "");
-		const fileSize = parseInt(
-			header.toString("utf-8", 124, 136).replace(/\0.*/g, ""),
-			8
-		);
-
-		if (fileName === filepath) {
-			return tarballBuffer.subarray(offset, offset + fileSize);
-		}
-
-		// Clamp offset to the uppoer multiple of 512
-		offset = (offset + fileSize + 511) & ~511;
+		});
+		await unpackPromise;
+	} catch (error) {
+		console.error("Error downloading binary from GitHub:", error);
 	}
 }
 
-async function downloadBinaryFromNpm() {
-	// Download the tarball of the right binary distribution package
-	const tarballDownloadBuffer = await makeRequest(
-		`https://registry.npmjs.org/${binaryPackageName}/-/${binaryPackageName}-${BINARY_DISTRIBUTION_VERSION}.tgz`
-	);
-	const tarballBuffer = lzmajs.decompressFile(tarballDownloadBuffer);
-	// const tarballBuffer = zlib.unzipSync(tarballDownloadBuffer)
-
-	// Extract binary from package and write to disk
-	fs.writeFileSync(
-		fallbackBinaryPath,
-		extractFileFromTarball(tarballBuffer, `package/bin/${binaryName}`),
-		{ mode: 0o755 } // Make binary file executable
-	);
-}
-
-function isPlatformSpecificPackageInstalled() {
+function isDataDirDownloaded() {
 	try {
-		// Resolving will fail if the optionalDependency was not installed
-		require.resolve(`${binaryPackageName}/bin/${binaryName}`);
-		return true;
+		return existsSync(path.join(unpackPath, "data"));
 	} catch (e) {
 		return false;
 	}
 }
 
-if (!binaryPackageName) {
-	throw new Error("Platform not supported!");
-}
-
 // Skip downloading the binary if it was already installed via optionalDependencies
-if (!isPlatformSpecificPackageInstalled()) {
-	console.log(
-		"Platform specific package not found. Will manually download binary."
-	);
-	downloadBinaryFromNpm();
+if (!isDataDirDownloaded()) {
+	console.log("Data directory not found. Will download from GitHub.");
+	downloadBinaryFromGitHub();
 } else {
-	console.log(
-		"Platform specific package already installed. Will fall back to manually downloading binary."
-	);
+	console.log("Data directory already installed.");
 }
